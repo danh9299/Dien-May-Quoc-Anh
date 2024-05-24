17fad2a46abdeabbe1ce90e7900d6492715cf8a9
unknown <nguyenduyanh.tit@gmail.com>
2024-05-24 15:34:37 +0700

    login for users

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,6 +5,7 @@ use App\Models\Image;
 use App\Models\Catalog;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
 class LoginController extends Controller
 {
     //
@@ -12,5 +13,24 @@ class LoginController extends Controller
         $logo = Image::where('group',1)->first();
         $main_catalogs = Catalog::where('parent_id',0)->get();
         return view('main.auth.login', ['logo'=>$logo, 'main_catalogs'=>$main_catalogs]);}
+    
+        public function authenticate(Request $request): RedirectResponse
+        {
+            $credentials = $request->validate([
+                'email' => ['required'],
+                'password' => ['required'],
+            ]);
+     
+            if (Auth::guard('web')->attempt($credentials)) {
+                $request->session()->regenerate();
+     
+                return redirect()->intended(route('main.home'));
+            }
+     
+            return back()->withErrors([
+                'username' => 'Thông tin đăng nhập không chính xác.',
+            ])->onlyInput('password');
+        }
+    
     }
 
